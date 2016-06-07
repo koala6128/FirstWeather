c2c5285f094dd70d46dd582f177901b3c2c9eb8e
Koala <nwulx@126.com>
2016-06-07 18:16:02 +0800

    城市天气界面改为fragment

--- app/src/main/java/com/example/lx/firstweather/MainActivity.java
+++ app/src/main/java/com/example/lx/firstweather/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.lx.firstweather;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.lx.firstweather.ui.Detail_fragment;
+import com.example.lx.firstweather.ui.Summary_fragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Detail_fragment detailFragment = new Detail_fragment();
+        Summary_fragment summaryFragment = new Summary_fragment();
+
+        FragmentManager manager = getFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.fragment_container0, detailFragment);
+        transaction.add(R.id.fragment_container1, summaryFragment);
+        transaction.commit();
+
+
     }
 }
