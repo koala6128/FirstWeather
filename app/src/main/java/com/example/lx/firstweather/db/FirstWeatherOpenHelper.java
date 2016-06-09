3104d24bed7b87d8fa46910a82b294459972a64f
Koala <nwulx@126.com>
2016-06-09 19:56:10 +0800

    新增数据库帮助类，以及city、weather表对应的类

---
+++ app/src/main/java/com/example/lx/firstweather/db/FirstWeatherOpenHelper.java
@@ -0,0 +1,7 @@
+package com.example.lx.firstweather.db;
+
+/**
+ * Created by koala on 2016/6/9.
+ */
+public class FirstWeatherOpenHelper {
+}
---
+++ app/src/main/java/com/example/lx/firstweather/model/Weather.java
@@ -0,0 +1,7 @@
+package com.example.lx.firstweather.model;
+
+/**
+ * Created by koala on 2016/6/9.
+ */
+public class Weather {
+}
